131d50fb72f3ed81aada77ec9fb07eab0fbafe5b
Juliano dos Santos <judossantoos@gmail.com>
2024-06-12 21:30:39 -0300

    adjusted structure of frontend classes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import './style.css'
+
+function Home() {
+
+    return (
+        <div className='container'>
+            <form>
+            <h1>Product Registration</h1>
+            <input name='description' />
+            </form>
+        </div>
+    )
+}
+
+export default Home
+
